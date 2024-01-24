d32b3071bda8f7c8d5e9e650072c858db637bd34
49372s <kyouyouidsiyou@gmail.com>
2024-01-24 21:14:32 +0900

    fix: flutter analyzeで指摘された数か所を修正。まだまだ先は長い。

--- lib/api/reaction.dart
+++ lib/api/reaction.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:twisskey/api/myAccount.dart';
@@ -12,7 +13,9 @@ class DoReaction{
     final body = {"noteId": noteId, "reaction":emojiCode,"i":token};
     final response = await http.post(uri,headers: headers, body: jsonEncode(body));
     final res = response.body;
-    print(res);
+    if (kDebugMode) {
+      print(res);
+    }
   }
   Future<bool> check(String noteId, String emojiCode) async {
     var token = await sysAccount().getToken();
@@ -26,14 +29,13 @@ class DoReaction{
     final String res = response.body;
     List<dynamic> list = jsonDecode(res);
     bool flug = false;
-    if(list!=[] && list!=null && list != "") {
+    if(list!=[] && list != "") {
       Map<String, dynamic> map = {};
       list.forEach((element) =>
       {
         map["users"] = element["user"]["id"],
         map["type"] = element["type"],
         if(map["users"] == id){
-          print("リアクション済み"),
           if (emojiCode == map["type"]) {
             flug = true,
             Fluttertoast.showToast(msg: "同一のリアクションがあります")
@@ -47,7 +49,7 @@ class DoReaction{
     return flug;
   }
 
-  Future<String> get(String noteId) async {
+  Future<Map<String,dynamic>> get(String noteId) async {
     var token = await sysAccount().getToken();
     var host = await sysAccount().getHost();
     var info = await sysAccount().getUserInfo();
@@ -58,15 +60,15 @@ class DoReaction{
     final response = await http.post(uri,headers: headers, body: jsonEncode(body));
     final String res = response.body;
     List<dynamic> list = jsonDecode(res);
-    String a = "";
-    if(list!=[] && list!=null && list != "") {
+    Map<String,dynamic> a = {"status":"no","reactions":list.length.toString()};
+    if(list!=[] && list != "") {
       Map<String, dynamic> map = {};
       list.forEach((element) =>
       {
         map["users"] = element["user"]["id"],
         map["type"] = element["type"],
         if (map["users"] == id) {
-          a = "reactioned",
+          a = {"status":"yes","reactions":list.length.toString()},
         }
       });
       return a;
@@ -78,8 +80,6 @@ class DoReaction{
   Future<String> getReactions(String noteId) async {
     var token = await sysAccount().getToken();
     var host = await sysAccount().getHost();
-    var info = await sysAccount().getUserInfo();
-    var id = info["id"];
     final Uri uri = Uri.parse("https://$host/api/notes/reactions");
     Map<String, String> headers = {'Content-Type': 'application/json',"charset":'UTF-8'};
     final body = {"noteId": noteId,"i":token};
